02e1d6a4715d11ec722ad0033c1b922ab09fa53b
Jon Speicher <jon.speicher@gmail.com>
2013-12-30 15:57:49 -0500

    Clean up variable names in solid.py

--- blinkylib/patterns/solid.py
+++ blinkylib/patterns/solid.py
@@ -1,12 +1,12 @@
 import blinkypattern
 
 class Solid(blinkypattern.BlinkyPattern):
-    def __init__(self, blinkytape, rgb):
+    def __init__(self, blinkytape, solid_rgb):
         super(Solid, self).__init__(blinkytape)
-        self._rgb = rgb
+        self._solid_rgb = solid_rgb
 
     def setup(self):
         super(Solid, self).setup()
-        pixels = [self._rgb] * self._blinkytape.pixel_count
+        pixels = [self._solid_rgb] * self._blinkytape.pixel_count
         self._blinkytape.set_pixels(pixels)
         self._blinkytape.update()

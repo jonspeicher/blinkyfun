20de732f1a65dd74e17bf95e57f9aba1123887b4
Jon Speicher <jon.speicher@gmail.com>
2013-12-30 14:47:53 -0500

    Move patterns to package

--- pilotlight.py
+++ pilotlight.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import gradient
+from patterns.gradient import Gradient
 from blinkytape import BlinkyTape
 
 bt = BlinkyTape('/dev/tty.usbmodem1421')
-pattern = gradient.Gradient(bt, BlinkyTape.BLUE, BlinkyTape.WHITE)
+pattern = Gradient(bt, BlinkyTape.BLUE, BlinkyTape.WHITE)
 pattern.setup()

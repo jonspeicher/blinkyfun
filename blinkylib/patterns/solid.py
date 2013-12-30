ebeb610bdaf0b5c9c11f359e1c9756652496ce47
Jon Speicher <jon.speicher@gmail.com>
2013-12-30 17:19:40 -0500

    Clean up solid pattern; do work in init, not setup

--- blinkylib/patterns/solid.py
+++ blinkylib/patterns/solid.py
@@ -3,10 +3,9 @@ import blinkypattern
 class Solid(blinkypattern.BlinkyPattern):
     def __init__(self, blinkytape, solid_color):
         super(Solid, self).__init__(blinkytape)
-        self._solid_color = solid_color
+        self._pixels = [solid_color] * self._blinkytape.pixel_count
 
     def setup(self):
         super(Solid, self).setup()
-        pixels = [self._solid_color] * self._blinkytape.pixel_count
-        self._blinkytape.set_pixels(pixels)
+        self._blinkytape.set_pixels(self._pixels)
         self._blinkytape.update()

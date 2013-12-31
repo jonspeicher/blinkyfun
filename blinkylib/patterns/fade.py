bb247025d1bccdc8b41d5a92ee4db2cbf67b8107
Jon Speicher <jon.speicher@gmail.com>
2013-12-31 16:11:43 -0500

    Refactor fade logic a bit to isolate ramp production

--- blinkylib/patterns/fade.py
+++ blinkylib/patterns/fade.py
@@ -1,4 +1,5 @@
 import blinkypattern
+import itertools
 
 # TBD: TrapezoidalFade vs SinusoidalFade?
 class Fade(blinkypattern.BlinkyPattern):
@@ -16,8 +17,22 @@ class Fade(blinkypattern.BlinkyPattern):
         self._index = (self._index + 1) % len(self._profile)
 
     def _make_fade_profile(self, color_list, period_sec):
-        steps_per_period = period_sec / self._timebase_sec
+        steps_per_period = int(period_sec / self._timebase_sec)
+        circular_color_list = color_list + [color_list[0]]
         profile = []
-        for color in color_list:
-            profile.extend([color] * int(steps_per_period))
+        for this_color, next_color in self._pairwise(circular_color_list):
+            ramp = self._make_color_ramp(this_color, next_color, steps_per_period)
+            constant = self._make_constant_color(next_color, steps_per_period)
+            profile.extend(ramp + constant)
         return profile
+
+    def _make_constant_color(self, color, count):
+        return [color] * count
+
+    def _make_color_ramp(self, first_color, second_color, step_count):
+        return []
+
+    def _pairwise(self, iterable):
+        this_iter, next_iter = itertools.tee(iterable)
+        next(next_iter)
+        return itertools.izip(this_iter, next_iter)

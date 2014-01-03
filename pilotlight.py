f64967d195089b4f0c6bb960cc44a24d57de0889
Jon Speicher <jon.speicher@gmail.com>
2014-01-03 02:25:51 -0500

    Make pilotlight a rainbow gradient for now

--- pilotlight.py
+++ pilotlight.py
@@ -4,8 +4,10 @@ from blinkylib import blinkytape, blinkycolor
 from blinkylib.patterns import fade
 import time
 
-bt = blinkytape.BlinkyTape('/dev/tty.usbmodem1421')
-color_list = [blinkycolor.BLACK, blinkycolor.BLUE, blinkycolor.WHITE]
+bt = blinkytape.BlinkyTape('/dev/tty.usbmodem1411')
+color_list = [blinkycolor.BLACK, blinkycolor.RED, blinkycolor.ORANGE,
+        blinkycolor.YELLOW, blinkycolor.GREEN, blinkycolor.BLUE,
+        blinkycolor.PURPLE, blinkycolor.WHITE]
 pattern = fade.Fade(bt, color_list, 1)
 pattern.setup()
 

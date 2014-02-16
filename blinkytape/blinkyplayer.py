c0633bc60dda6b81e623795f2c65a1eb0ba5933d
Jon Speicher <jon.speicher@gmail.com>
2014-02-16 17:32:03 -0500

    Clean up BlinkyPlayer a little

--- blinkytape/blinkyplayer.py
+++ blinkytape/blinkyplayer.py
@@ -7,7 +7,7 @@ class BlinkyPlayer(object):
         self._blinkytape = blinkytape
 
     def play(self, animation, num_cycles = FOREVER):
-        finished = self._make_finished_predicate(animation, num_cycles)
+        finished = self._finished_predicate(animation, num_cycles)
         animation.begin()
         while not finished():
             pixels = animation.next_frame()
@@ -15,14 +15,21 @@ class BlinkyPlayer(object):
             time.sleep(animation.frame_period_sec)
         animation.end()
 
-    def _make_finished_predicate(self, animation, num_cycles):
+    def _finished_predicate(self, animation, num_cycles):
         if num_cycles < 0 and num_cycles != self.FOREVER: raise ValueError
         if num_cycles == self.FOREVER:
-            predicate = lambda: False
+            predicate = self._forever_predicate()
         else:
             self._num_frames = animation.frame_count * num_cycles
-            def predicate():
-                finished = self._num_frames <= 0
-                self._num_frames = self._num_frames - 1
-                return finished
+            predicate = self._frame_count_predicate()
+        return predicate
+
+    def _forever_predicate(self):
+        return lambda: False
+
+    def _frame_count_predicate(self):
+        def predicate():
+            finished = self._num_frames <= 0
+            self._num_frames = self._num_frames - 1
+            return finished
         return predicate

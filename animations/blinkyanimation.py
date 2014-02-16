a10dd9795efe8aaa089daf8c8e4d5d455fefef70
Jon Speicher <jon.speicher@gmail.com>
2014-02-16 17:26:02 -0500

    Add new animations objects

---
+++ animations/blinkyanimation.py
@@ -0,0 +1,22 @@
+class BlinkyAnimation(object):
+    def __init__(self, frame_count, frame_period_sec):
+        if frame_count < 0 or frame_period_sec < 0: raise ValueError
+        self._frame_count = frame_count
+        self._frame_period_sec = frame_period_sec
+
+    @property
+    def frame_count(self):
+        return self._frame_count
+
+    @property
+    def frame_period_sec(self):
+        return self._frame_period_sec
+
+    def begin(self):
+        pass
+
+    def next_frame(self):
+        pass
+
+    def end(self):
+        pass

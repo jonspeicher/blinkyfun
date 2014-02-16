a92121cfdbb94d36d021fb8d1386031829ee86a2
Jon Speicher <jon.speicher@gmail.com>
2014-02-16 03:33:16 -0500

    Update Solid pattern for refactor

--- patterns/solid.py
+++ patterns/solid.py
@@ -1,11 +1,7 @@
-import blinkypattern
+class Solid(object):
+    def __init__(self, pixel_count, color):
+        self._pixels = [color] * pixel_count
 
-class Solid(blinkypattern.BlinkyPattern):
-    def __init__(self, blinkytape, solid_color):
-        super(Solid, self).__init__(blinkytape)
-        self._pixels = [solid_color] * self._blinkytape.pixel_count
-
-    def setup(self):
-        super(Solid, self).setup()
-        self._blinkytape.set_pixels(self._pixels)
-        self._blinkytape.update()
+    @property
+    def pixels(self):
+        return self._pixels

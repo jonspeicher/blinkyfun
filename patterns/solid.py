0b668fc3eba999ca760322d000b929eac0931b18
Jon Speicher <jon.speicher@gmail.com>
2014-02-17 00:04:51 -0500

    Changed my mind; pass pixel count directly in to patterns

--- patterns/solid.py
+++ patterns/solid.py
@@ -1,6 +1,6 @@
 class Solid(object):
-    def __init__(self, config, color):
-        self._pixels = [color] * config.pixel_count
+    def __init__(self, pixel_count, color):
+        self._pixels = [color] * pixel_count
 
     @property
     def pixels(self):
--- steady-color.py
+++ steady-color.py
@@ -7,6 +7,7 @@ import sys
 tape = blinkytape.BlinkyTape.find_first()
 
 color = blinkycolor.BlinkyColor.from_string(sys.argv[1])
-pattern = solid.Solid(tape, color)
+pattern = solid.Solid(tape.pixel_count, color)
+
 player = blinkyplayer.BlinkyPlayer(tape)
 player.display_pattern(pattern)

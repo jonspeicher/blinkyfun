18320d0ba71a76f86c17da9a2a2f14c59e906872
Jon Speicher <jon.speicher@gmail.com>
2014-02-17 01:06:44 -0500

    Rename some methods in gradient

--- patterns/gradient.py
+++ patterns/gradient.py
@@ -2,19 +2,19 @@ from blinkytape import blinkycolor
 
 class Gradient(object):
     def __init__(self, pixel_count, start_color, end_color):
-        self._pixels = self._make_rgb_gradient(pixel_count, start_color, end_color)
+        self._pixels = self._rgb_gradient(pixel_count, start_color, end_color)
 
     @property
     def pixels(self):
         return self._pixels
 
-    def _make_rgb_gradient(self, pixel_count, start_color, end_color):
-        red_gradient = self._make_gradient(start_color.red, end_color.red, pixel_count)
-        green_gradient = self._make_gradient(start_color.green, end_color.green, pixel_count)
-        blue_gradient = self._make_gradient(start_color.blue, end_color.blue, pixel_count)
+    def _rgb_gradient(self, pixel_count, start_color, end_color):
+        red_gradient = self._gradient(start_color.red, end_color.red, pixel_count)
+        green_gradient = self._gradient(start_color.green, end_color.green, pixel_count)
+        blue_gradient = self._gradient(start_color.blue, end_color.blue, pixel_count)
         rgb_gradient = zip(red_gradient, green_gradient, blue_gradient)
         return [blinkycolor.BlinkyColor(*rgb) for rgb in rgb_gradient]
 
-    def _make_gradient(self, start, end, count):
+    def _gradient(self, start, end, count):
         delta = (end - start) / float(count - 1)
         return [start + (delta * index) for index in range(0, count)]
